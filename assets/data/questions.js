7e5226e18ec0c071470c1ecaa90b7f3086ba7cc5
HYF <alinamarasca@gmail.com>
2022-01-10 12:19:57 +0100

    add hints and change questions to be more precise

--- assets/data/questions.js
+++ assets/data/questions.js
@@ -1,26 +1,30 @@
 let questions = [
   {
     question: "What is tomato?",
-    answers: ["vegetable", "fruit", "berry", "taproot"],
+    answers: ["vegetable", "leafy greens", "berry", "taproot"],
     correct: "berry",
+    hint: "Tomato is considered a 'culinary vegetable', but botanically it is not!",
     id: 1
   },
   {
     question: "How do you call baby falcon?",
-    answers: ["pup", "chick", "fledgling", "joey"],
-    correct: "chick",
+    answers: ["pup", "eyas", "owlet", "gosling"],
+    correct: "eyas",
+    hint: "This is how gosling looks like:",
     id: 2
   },
   {
     question: "How big is the largest frog?",
     answers: ["900 grams", "1,6 kilograms", "500 grams", "3 kilograms"],
     correct: "3 kilograms",
+    hint: "The largest frog looks like this!",
     id: 3
   },
   {
     question: "Where does the sun rise?",
     answers: ["east", "west", "north", "south"],
     correct: "east",
+    hint: "Japan is often called 'the land of the rising sun.'",
     id: 4
   }
 ];
